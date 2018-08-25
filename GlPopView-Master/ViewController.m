59815177968b6146f38beee03c89e9392386acb5
gleeeli <gleeeli@sina.com>
2018-08-25 16:14:08 +0800

    一个气泡框
    支持隐藏箭头，支持各种方向显示，支持四周圆角分别设置显示

--- GlPopView-Master/ViewController.m
+++ GlPopView-Master/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "GlPopView.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    CGFloat popY = 100;
+    GlPopView *popView = [[GlPopView alloc] initWithFrame:CGRectMake(100, popY + 10, 100, 150)];
+    [self.view addSubview:popView];
 }
 
 
